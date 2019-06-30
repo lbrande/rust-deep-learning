c4b97a23ddb84c70a61428298733fd9789e3c649
= <=>
2019-06-30 13:26:33 +0200

    Cleanup base code.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -5,47 +5,28 @@ use rand_distr::StandardNormal;
 type Vector = na::DVector<f64>;
 type Matrix = na::DMatrix<f64>;
 
-trait FunctionalList<T> {
-    fn map<U>(&self, function: &Fn(&T) -> U) -> Vec<U>;
-    fn zip_map<U>(&self, other: &Self, function: &Fn(&T, &T) -> U) -> Vec<U>;
+fn map<T: Copy, U>(f: &Fn(T) -> U, slice: &[T]) -> Vec<U> {
+    slice.iter().map(|&t| f(t)).collect()
 }
 
-impl<T> FunctionalList<T> for [T] {
-    fn map<U>(&self, function: &Fn(&T) -> U) -> Vec<U> {
-        let mut result = Vec::with_capacity(self.len());
-        for t in self {
-            result.push(function(t));
-        }
-        result
-    }
-
-    fn zip_map<U>(&self, other: &Self, function: &Fn(&T, &T) -> U) -> Vec<U> {
-        let mut result = Vec::with_capacity(self.len());
-        for i in 0..self.len() {
-            result.push(function(&self[i], &other[i]));
-        }
-        result
-    }
+fn zip_with<T: Copy, U>(f: &Fn(T, T) -> U, slice_0: &[T], slice_1: &[T]) -> Vec<U> {
+    slice_0
+        .iter()
+        .zip(slice_1)
+        .map(|(&t_0, &t_1)| f(t_0, t_1))
+        .collect()
 }
 
 fn randn() -> f64 {
     thread_rng().sample(StandardNormal)
 }
 
-fn vector(len: usize, generator: &Fn() -> f64) -> Vector {
-    let mut result = Vec::with_capacity(len);
-    for _ in 0..len {
-        result.push(generator());
-    }
-    na::DVector::from_vec(result)
+fn randn_vector(rows: usize) -> Vector {
+    Vector::from_vec((0..rows).map(|_| randn()).collect())
 }
 
-fn matrix(rows: usize, columns: usize, generator: &Fn() -> f64) -> Matrix {
-    let mut result = Vec::with_capacity(rows * columns);
-    for _ in 0..rows * columns {
-        result.push(generator());
-    }
-    na::DMatrix::from_vec(rows, columns, result)
+fn randn_matrix(rows: usize, columns: usize) -> Matrix {
+    Matrix::from_vec(rows, columns, (0..rows*columns).map(|_| randn()).collect())
 }
 
 pub struct Network {
@@ -58,8 +39,8 @@ impl Network {
         let init = &layers[1..];
         let tail = &layers[..layers.len() - 1];
         Self {
-            biases: init.map(&|&x| vector(x, &randn)),
-            weights: tail.zip_map(init, &|&x, &y| matrix(x, y, &randn)),
+            biases: map(&randn_vector, init),
+            weights: zip_with(&randn_matrix, tail, init),
         }
     }
 }
