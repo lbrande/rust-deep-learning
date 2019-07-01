b986f230ce85d76117f7004a27cb9cdffc3147f0
= <=>
2019-07-01 11:01:35 +0200

    Cleaned up code.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,52 +1,13 @@
-use nalgebra as na;
-use rand::prelude::*;
-use rand::seq::SliceRandom;
-use rand_distr::StandardNormal;
-use std::iter::Sum;
+mod utils;
 
-type Vector = na::DVector<f64>;
-type Matrix = na::DMatrix<f64>;
-type Data = (Vector, Vector);
+use nalgebra::*;
+use utils::*;
 
-fn map<T: Copy, U>(f: &Fn(T) -> U, slice: &[T]) -> Vec<U> {
-    slice.iter().map(|&t| f(t)).collect()
-}
-
-fn map_and_sum<T, U: Sum>(f: &Fn(&T) -> U, slice: &[T]) -> U {
-    slice.iter().map(|t| f(t)).sum()
-}
-
-fn zip_with<T: Copy, U>(f: &Fn(T, T) -> U, slice_0: &[T], slice_1: &[T]) -> Vec<U> {
-    slice_0
-        .iter()
-        .zip(slice_1)
-        .map(|(&t_0, &t_1)| f(t_0, t_1))
-        .collect()
-}
-
-fn randn() -> f64 {
-    thread_rng().sample(StandardNormal)
-}
-
-fn shuffle<T>(slice: &mut [T]) {
-    slice.shuffle(&mut thread_rng())
-}
-
-fn randn_vector(rows: usize) -> Vector {
-    Vector::from_vec((0..rows).map(|_| randn()).collect())
-}
-
-fn randn_matrix(rows: usize, columns: usize) -> Matrix {
-    Matrix::from_vec(
-        rows,
-        columns,
-        (0..rows * columns).map(|_| randn()).collect(),
-    )
-}
+type Data = (DVector<f64>, DVector<f64>);
 
 pub struct Network {
-    biases: Vec<Vector>,
-    weights: Vec<Matrix>,
+    biases: Vec<DVector<f64>>,
+    weights: Vec<DMatrix<f64>>,
 }
 
 impl Network {
@@ -54,8 +15,8 @@ impl Network {
         let init = &layers[1..];
         let tail = &layers[..layers.len() - 1];
         Self {
-            biases: map(&randn_vector, init),
-            weights: zip_with(&randn_matrix, tail, init),
+            biases: map(&|&x| randn_vector(x), init),
+            weights: zip_with(&|&x, &y| randn_matrix(x, y), tail, init),
         }
     }
 
@@ -64,7 +25,7 @@ impl Network {
         epochs: usize,
         batch_size: usize,
         learning_rate: f64,
-        training_data: &mut Vec<Data>,
+        training_data: &mut [Data],
         test_data: Option<&[Data]>,
     ) {
         for i in 0..epochs {
@@ -74,7 +35,12 @@ impl Network {
                 self.train_batch(batch, learning_rate);
             }
             if let Some(test_data) = test_data {
-                println!("Epoch {}: {} / {}", i, self.evaluate(test_data), test_data.len());
+                println!(
+                    "Epoch {}: {} / {}",
+                    i,
+                    self.evaluate(test_data),
+                    test_data.len()
+                );
             } else {
                 println!("Epoch {} complete", i);
             }
@@ -84,7 +50,19 @@ impl Network {
     fn train_batch(&mut self, batch: &[Data], learning_rate: f64) {}
 
     fn evaluate(&self, test_data: &[Data]) -> usize {
-        map_and_sum(&|(x, y)| (x == y) as usize, test_data)
+        sum_by(
+            &|(x, y): &Data| (self.feedforward(x).imax() == y.imax()) as usize,
+            test_data,
+        )
+    }
+
+    fn feedforward(&self, input: &DVector<f64>) -> DVector<f64> {
+        let (b, w) = (&self.biases[0], &self.weights[0]);
+        let mut a = w * input + b;
+        for (b, w) in zip(&self.biases[1..], &self.weights[1..]) {
+            a = w * a + b
+        }
+        a
     }
 }
 
